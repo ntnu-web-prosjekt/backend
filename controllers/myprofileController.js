c3c0ddf103796879d7f9d1edb9461cc97fada02b
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-03-31 16:12:32 +0200

    getUserInfo & getUserDetails

--- controllers/myprofileController.js
+++ controllers/myprofileController.js
@@ -1,3 +1,41 @@
+const User = require("../schemas/myProfileSchema.js");
+
+const getUserInfo = async (req, res) => {
+  try {
+    const userInfo = await User.findById(req.body.id).select(
+      "name email phone degree university tags description"
+    );
+
+    if (!userInfo) {
+      res.status(400);
+      throw new Error("There exists no data yet!");
+    } else {
+      res.json(userInfo);
+    }
+  } catch (error) {
+    res.json({
+      msg: error.message,
+    });
+  }
+};
+
+const getUserDetails = async (req, res) => {
+  try {
+    const userInfo = await User.findById(req.body.id);
+
+    if (!userInfo) {
+      res.status(400);
+      throw new Error("There exists no data yet!");
+    } else {
+      res.json(userInfo);
+    }
+  } catch (error) {
+    res.json({
+      msg: error.message,
+    });
+  }
+};
+
 const doSomeTest = async (req, res) => {
   try {
     const testResult = await Test.find();
@@ -17,4 +55,6 @@ const doSomeTest = async (req, res) => {
 
 module.exports = {
   doSomeTest,
+  getUserInfo,
+  getUserDetails,
 };
--- routers/myprofileRouter.js
+++ routers/myprofileRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { doSomeTest } = require("../controllers/myprofileController.js");
+const {
+  doSomeTest,
+  getUserInfo,
+  getUserDetails,
+} = require("../controllers/myprofileController.js");
 
 // GET - retrives the public data about the logged in user
-router.get("/view", doSomeTest);
+router.get("/view", getUserInfo);
 
 // GET - retrives all data about the logged in user for making changes
-router.get("/settings", doSomeTest);
+router.get("/settings", getUserDetails);
 
 // PUT - saves changes made to the signed in users profile data
 router.put("/settings/update/data", doSomeTest);
