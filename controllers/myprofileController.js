98d6387d5c0b3674081625698406235583654380
Magnus <69847549+Magnumoe@users.noreply.github.com>
2022-03-31 23:53:17 +0200

    added Update Details, needs check

--- controllers/myprofileController.js
+++ controllers/myprofileController.js
@@ -73,10 +73,27 @@ const updateUserPwd = async (req, res) => {
   }
 };
 
-// not finished yet, need to set update values
+// works, but sends back old data in Postman, use GetUserDetails to check for changes
 const updateUserInfo = async (req, res) => {
+  const updateQuery = {};
+
+  if (!req.body.details) {
+    res.status(400).send("No details to update");
+    return;
+  }
+
+  if (req.body.details) {
+    updateQuery.details = req.body.details;
+  } else {
+    res.status(403).send("Something went wrong, please try again later");
+    return;
+  }
+
   try {
-    const updateInfo = await User.updateOne({ _id: req.body.id }, { $set: {} });
+    const updateInfo = await User.findByIdAndUpdate(req.body.id, {
+      $set: updateQuery.details,
+    });
+    console.log(req.body.details);
     res.json(updateInfo);
   } catch (error) {
     res.status(400).send(error.message);
