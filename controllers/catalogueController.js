e8dc119599740bdcb9bc3c005329747df9852ca8
Henrik Granum <henriklg@stud.ntnu.no>
2022-04-05 20:20:15 +0200

    initial viewRequest and applyRequest

--- controllers/catalogueController.js
+++ controllers/catalogueController.js
@@ -1,3 +1,5 @@
+const Request = require("../schemas/requestsSchema.js");
+
 const doSomeTest = async (req, res) => {
   try {
     const testResult = await Test.find();
@@ -15,6 +17,53 @@ const doSomeTest = async (req, res) => {
   }
 };
 
+/**
+* Retrieves data for a specific request from the database as JSON.
+*
+* @param Object $req The request object
+* @param Object $res The response object
+*/
+const viewRequest = async (req, res) => {
+  try {
+    // Retrieving request data
+    const requestDetails = await Request.findOne({ _id: req.params.id });
+
+    // Checking if the request exists
+    if (!requestDetails) {
+      res.status(400).json({ "msg": "This request ID does not exist." });
+      return;
+    }
+
+    // Checks if the user already applied for this request
+    if (requestDetails.examinatorId.includes(req.body.user_id)) {
+      // Returns request details, plus indicator that already applied
+      res.status(200).json({ requestDetails, alreadyApplied: true });
+      return;
+
+      // The user have not applied for this request yet, returns request details
+    } else {
+      res.status(200).json({ requestDetails });
+      return;
+    }
+  } catch (error) {
+    res.status(400).json({ "msg": error.message });
+  }
+};
+
+/**
+* Makes the logged in user apply for the request they viewing. Will be stored in the DB.
+*
+* @param Object $req The request object
+* @param Object $res The response object
+*/
+const applyRequest = async (req, res) => {
+  try {
+  
+  } catch (error) {
+    res.status(400).json({ "msg": error.message });
+  }
+};
+
 module.exports = {
-  doSomeTest,
+  doSomeTest,viewRequest,applyRequest
 };
--- routers/catalogueRouter.js
+++ routers/catalogueRouter.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { doSomeTest } = require("../controllers/catalogueController.js");
+const { doSomeTest, viewRequest, applyRequest } = require("../controllers/catalogueController.js");
 
 // GET - Retrieves all open requests
 router.get("/view", doSomeTest);
 
 // GET - Retrieves info about single request, checks if the signed in user have already applied for the request
-router.get("/view/:id", doSomeTest);
+router.get("/view/:id", viewRequest);
 
-// POST - Signed in user sends application for the request they are viewing
-router.post("/apply/:id", doSomeTest);
+// PUT - Signed in user sends application for the request they are viewing
+router.put("/apply/:id", applyRequest);
 
 module.exports = router;
