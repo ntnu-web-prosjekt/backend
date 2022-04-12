27b409b4177dc75f5336e2fed7b798ab76443d68
Henrik Granum <henriklg@stud.ntnu.no>
2022-04-12 11:14:31 +0200

    added logic for getting statistics

--- routers/statsRouter.js
+++ routers/statsRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { doSomeTest } = require("../controllers/statsController.js");
+const { getStats } = require("../controllers/statsController.js");
 
 // GET - Retrieve open request and matched requests data (shown on the front page before logging in)
-router.get("/view", doSomeTest);
+router.get("/view", getStats);
 
 module.exports = router;
