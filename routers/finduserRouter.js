976e47c38be35663e63e3e90f4213f79b83ac269
Henrik Granum <henriklg@stud.ntnu.no>
2022-04-21 12:37:11 +0200

    find user functionality

--- routers/finduserRouter.js
+++ routers/finduserRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { doSomeTest } = require("../controllers/finduserController.js");
+const { getAllUsers, getSpecificUser, doSomeTest } = require("../controllers/finduserController.js");
 
 // GET - Retrieves all users name, university, title, fulfilled requests and tags
-router.get("/view", doSomeTest);
+router.get("/view", getAllUsers);
 
 // GET - View the user profile of a specific user and retrieve the signed
 //       in users requests which does not yet have a second examinator
-router.get("/view/:id", doSomeTest);
+router.get("/view/:id", getSpecificUser);
 
 // POST - Send a request inquiry to the specific user
 router.post("/inquiry");
