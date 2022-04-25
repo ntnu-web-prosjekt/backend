ea354cd0ee68fb7152c963089bf5167217616a14
Henrik <72784131+henriklgntnu@users.noreply.github.com>
2022-04-25 16:11:30 +0200

    added params

--- routers/dashboardRouter.js
+++ routers/dashboardRouter.js
@@ -4,6 +4,6 @@ const { getDashboardData } = require("../controllers/dashboardController.js");
 
 // GET - Retrieves the upcoming examinations, pending examinator requests
 //       and total number of unanswered replies in the users inbox
-router.get("/view", getDashboardData);
+router.get("/view/:user_id", getDashboardData);
 
 module.exports = router;
